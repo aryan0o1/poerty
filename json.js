262e776b03c3efb93288d28125e9e9ca877a35bd
aryan0o1 <iaryan0o1@gmail.com>
2016-06-18 16:22:42 +0500

    Update json.js

--- json.js
+++ json.js
@@ -11,14 +11,6 @@
                     "title": "Google Fonts Website- Material Design"
                     
                     
-                },
-                {
-                    "id": 10564,
-                    "url": "http://stacktips.com/wp-content/uploads/2016/06/Google-Web-Fonts-Material-Design.png",
-                    
-                    "title": "Google Web Fonts- Material Design"
-                    
-                    
                 }
             ] 
         },
@@ -37,12 +29,67 @@
                     "title": "Google Fonts Website- Material Design"
                     
                     
-                },
+                }
+                ]
+    
+            
+            
+        },
+        {
+            
+            "url": "http://stacktips.com/tutorials/android/how-to-enable-deep-links-for-app-content-in-android",
+            
+            "title": "Quotes1",
+            
+            
+            "attachments": [
                 {
-                    "id": 10564,
-                    "url": "http://stacktips.com/wp-content/uploads/2016/06/Google-Web-Fonts-Material-Design.png",
+                    "id": 10563,
+                    "url": "https://3.bp.blogspot.com/-14_IlKFyEb0/VhZ9tj17yMI/AAAAAAAAE_Y/bppVkgn42dM/s400/11037633_1054163501262499_191414059278581851_n.jpg",
                     
-                    "title": "Google Web Fonts- Material Design"
+                    "title": "Quotes1"
+                    
+                    
+                }
+                ]
+    
+            
+            
+        },
+        {
+            
+            "url": "http://stacktips.com/tutorials/android/how-to-enable-deep-links-for-app-content-in-android",
+            
+            "title": "Quotes2",
+            
+            
+            "attachments": [
+                {
+                    "id": 10563,
+                    "url": "https://3.bp.blogspot.com/-DNYfMORYnM0/VhZ-IMmffEI/AAAAAAAAE_g/ejFEpa-5z8Y/s400/11046549_1052155698129946_7449005708985767886_n.jpg",
+                    
+                    "title": "Quotes2"
+                    
+                    
+                }
+                ]
+    
+            
+            
+        },
+        {
+            
+            "url": "http://stacktips.com/tutorials/android/how-to-enable-deep-links-for-app-content-in-android",
+            
+            "title": "Quotes3",
+            
+            
+            "attachments": [
+                {
+                    "id": 10563,
+                    "url": "https://3.bp.blogspot.com/-OMG7FHm2c8I/VhZ-4-NHMiI/AAAAAAAAE_s/5AsGuHI2F2k/s400/11048632_10153198468592863_6836975427856580888_n.jpg",
+                    
+                    "title": "Quotes3"
                     
                     
                 }
